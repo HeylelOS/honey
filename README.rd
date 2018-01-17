61fabe081f74a434808285fa5fd27081793b43f5
Valentin Debon <valentin@debon.net>
2018-01-17 10:30:37 +0100

    Structure of main utility

--- README.rd
+++ README.rd
@@ -1,9 +1,11 @@
+# Honey
 Honey is intended to be a unix-like OS package manager,
-archive structure format for packages and http remote
+archive structure format for packages and http/https remote
 package database system.
 
 It is meant to be composed of an ansi C library and
-a command line utility for now, plus a database guideline
-for following implementations.
+a command line utility for now, plus a guideline
+for future providers implementations.
 
 It keeps in mind the desire to stay simple and lightweight.
+
--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <stdarg.h>
 
-void fatal(const char *format, ...) {
+void honey_fatal(const char *format, ...) {
 	va_list args;
 	va_start(args, format);
 
@@ -15,91 +15,127 @@ void fatal(const char *format, ...) {
 	exit(EXIT_FAILURE);
 }
 
+void honey_fetch(int count, char **names) {
+	int i;
+
+	for(i = 0; i < count; i++) {
+		printf("fetch package %s\n", names[i]);
+	}
+}
+
+void honey_install(int count, char **files) {
+	int i;
+
+	for(i = 0; i < count; i++) {
+		printf("install file %s\n", files[i]);
+	}
+}
+
+void honey_list(char *method) {
+	if(strcmp(method, "providers") == 0) {
+		printf("list providers\n");
+	} else if(strcmp(method, "all") == 0) {
+		printf("list all\n");
+	} else if(strcmp(method, "active") == 0) {
+		printf("list active\n");
+	} else {
+		honey_fatal("error: list, \"%s\" is invalid\n", method);
+	}
+}
+
+void honey_remove(char *method, int count, char **names) {
+	int i;
+
+	if(strcmp(method, "total") == 0) {
+		printf("remove total\n");
+	} else if(strcmp(method, "partial") == 0) {
+		printf("remove partial\n");
+	} else {
+		honey_fatal("error: hny remove, \"%s\" is invalid\n", method);
+	}
+
+	for(i = 0; i < count; i++) {
+		printf("remove %s package %s\n", method, names[i]);
+	}
+}
+
+void honey_status(int count, char **names) {
+	int i;
+
+	for(i = 0; i < count; i++) {
+		printf("status package %s\n", names[i]);
+	}
+}
+
+void honey_repair(char *method, int count, char **names) {
+	int i;
+
+	if(strcmp(method, "all") == 0) {
+		printf("repair all\n");
+	} else if(strcmp(method, "clean") == 0) {
+		printf("repair clean\n");
+	} else if(strcmp(method, "check") == 0) {
+		printf("repair check\n");
+	} else if(strcmp(method, "config") == 0) {
+		printf("repair config\n");
+	} else {
+		honey_fatal("error: repair, \"%s\" is invalid\n", method);
+	}
+
+	for(i = 0; i < count; i++) {
+		printf("repair %s package %s\n", method, names[i]);
+	}
+}
+
 int main(int argc, char **argv) {
 	int i = 1;
-
-	if(argc < 2) {
-		fatal("error: expected action\n");
-	}
-
-	if(strcmp(argv[i], "fetch") == 0) {
-		i++;
-		if(i != argc) {
-			for(; i < argc; i++) {
-				printf("fetch package %s\n", argv[i]);
-			}
-		} else {
-			fatal("error: %s fetch [packages names...]\n", argv[0]);
-		}
-	} else if(strcmp(argv[i], "install") == 0) {
-		i++;
-		if(i != argc) {
-			for(; i < argc; i++) {
-				printf("install file %s\n", argv[i]);
-			}
-		} else {
-			fatal("error: %s install [packages files...]\n", argv[0]);
-		}
-	} else if(strcmp(argv[i], "list") == 0) {
-		i++;
-		if(i != argc) {
-			if(strcmp(argv[i], "providers") == 0) {
-				printf("list providers\n");
-			} else if(strcmp(argv[i], "all") == 0) {
-				printf("list all\n");
-			} else if(strcmp(argv[i], "active") == 0) {
-				printf("list active\n");
-			} else {
-				fatal("error: %s list, \"%s\" is invalid\n", argv[0], argv[i]);
-			}
-		} else {
-			fatal("error: %s list [providers | all | active]\n", argv[0]);
-		}
-	} else if(strcmp(argv[i], "remove") == 0) {
-		i++;
-		if(i != argc) {
-			if(strcmp(argv[i], "total") == 0) {
-				printf("remove total\n");
-			} else if(strcmp(argv[i], "partial") == 0) {
-				printf("remove partial\n");
-			} else {
-				fatal("error: %s remove, \"%s\" is invalid\n", argv[0], argv[i]);
-			}
-		} else {
-			fatal("error: %s remove [total | partial]\n", argv[0]);
-		}
-	} else if(strcmp(argv[i], "status") == 0) {
-		if(++i != argc) {
-			for(; i < argc; i++) {
-				printf("status package %s\n", argv[i]);
-			}
-		} else {
-			fatal("error: %s install [packages files...]\n", argv[0]);
-		}
-	} else if(strcmp(argv[i], "repair") == 0) {
-		i++;
-		if(i != argc) {
-			if(strcmp(argv[i], "all") == 0) {
-				printf("repair all\n");
-			} else if(strcmp(argv[i], "clean") == 0) {
-				printf("repair clean\n");
-			} else if(strcmp(argv[i], "check") == 0) {
-				printf("repair check\n");
-			} else if(strcmp(argv[i], "config") == 0) {
-				printf("repair config\n");
-			} else {
-				fatal("error: %s remove, \"%s\" is invalid\n", argv[0], argv[i]);
-			}
-		} else {
-			fatal("error: %s repair [all | clean | check | config]\n", argv[0]);
-		}
-	}
-
 	atexit(hny_disconnect);
 
 	/* if(hny_connect(HNY_CONNECT_WAIT) == HNY_ERROR_UNAVAILABLE) { */
 	if(hny_connect(0) == HNY_ERROR_UNAVAILABLE) {
-		fatal("hny error: unable to connect\n");
+		honey_fatal("hny error: unable to connect\n");
+	}
+
+	if(argc < 2) {
+		honey_fatal("error: expected action\n");
+	}
+
+	if(strcmp(argv[i], "fetch") == 0) {
+		if(++i != argc) {
+			honey_fetch(argc - i, &argv[i]);
+		} else {
+			honey_fatal("error: %s fetch [packages names...]\n", argv[0]);
+		}
+	} else if(strcmp(argv[i], "install") == 0) {
+		if(++i != argc) {
+			honey_install(argc - i, &argv[i]);
+		} else {
+			honey_fatal("error: %s install [packages files...]\n", argv[0]);
+		}
+	} else if(strcmp(argv[i], "list") == 0) {
+		if(++i == argc - 1) {
+			honey_list(argv[i]);
+		} else {
+			honey_fatal("error: %s list [providers | all | active]\n", argv[0]);
+		}
+	} else if(strcmp(argv[i], "remove") == 0) {
+		if(++i < argc - 1) {
+			honey_remove(argv[i], argc - i - 1, &argv[i + 1]);
+		} else {
+			honey_fatal("error: %s remove [total | partial] [packages names...]\n", argv[0]);
+		}
+	} else if(strcmp(argv[i], "status") == 0) {
+		if(++i != argc) {
+			honey_status(argc - i, &argv[i]);
+		} else {
+			honey_fatal("error: %s status [packages files...]\n", argv[0]);
+		}
+	} else if(strcmp(argv[i], "repair") == 0) {
+		if(++i < argc - 1) {
+			honey_repair(argv[i], argc - i - 1, &argv[i + 1]);
+		} else {
+			honey_fatal("error: %s repair [all | clean | check | config] [packages names...]\n", argv[0]);
+		}
 	}
 
 	exit(EXIT_SUCCESS);
