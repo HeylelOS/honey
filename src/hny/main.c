b34b3ed37ec9af97d0d8bad8fd9d8f1882695023
Valentin Debon <valentin@debon.net>
2019-07-15 13:13:57 +0200

    Status, list, everything usual

--- src/hny/main.c
+++ src/hny/main.c
@@ -2,14 +2,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <libgen.h>
+#include <dirent.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <err.h>
 
-struct hny_export_file {
+#define HNY_STATUS_BUFFER_DEFAULT_SIZE 64
+
+struct hny_extract_file {
 	const char *package;
 	const char *name;
 	size_t blocksize;
@@ -17,12 +22,12 @@ struct hny_export_file {
 };
 
 static void
-hny_export_file_init(struct hny_export_file *file,
+hny_extract_file_init(struct hny_extract_file *file,
 	char **argpos, char **argend) {
 
 	switch(argend - argpos) {
 	case 0:
-		errx(EXIT_FAILURE, "export: Expected arguments");
+		errx(EXIT_FAILURE, "extract: Expected arguments");
 	case 1: {
 		char *base = basename(*argpos);
 		char *dot = strrchr(base, '.');
@@ -35,24 +40,24 @@ hny_export_file_init(struct hny_export_file *file,
 		file->name = argpos[1];
 		break;
 	default:
-		errx(EXIT_FAILURE, "export: Too much arguments");
+		errx(EXIT_FAILURE, "extract: Too much arguments");
 	}
 
 	if((file->fd = open(file->name, O_RDONLY)) == -1) {
-		err(EXIT_FAILURE, "export: Unable to open %s", file->name);
+		err(EXIT_FAILURE, "extract: Unable to open '%s'", file->name);
 	}
 
 	file->blocksize = getpagesize();
 }
 
 static void
-hny_export(struct hny *hny, char **argpos, char **argend) {
+hny_command_extract(struct hny *hny, char **argpos, char **argend) {
 	struct hny_extraction *extraction;
-	struct hny_export_file file;
+	struct hny_extract_file file;
 
-	hny_export_file_init(&file, argpos, argend);
+	hny_extract_file_init(&file, argpos, argend);
 	if((errno = hny_extraction_create(&extraction, hny, file.package)) != 0) {
-		err(EXIT_FAILURE, "Unable to extract %s", file.name);
+		err(EXIT_FAILURE, "Unable to extract '%s'", file.name);
 	}
 
 	char buffer[file.blocksize];
@@ -62,10 +67,10 @@ hny_export(struct hny *hny, char **argpos, char **argend) {
 		switch(hny_extraction_extract(extraction, buffer, readval, &errcode)) {
 		case HNY_EXTRACTION_STATUS_ERROR_UNARCHIVE:
 			errno = errcode;
-			err(EXIT_FAILURE, "Unable to extract %s, error when unarchiving", file.name);
+			err(EXIT_FAILURE, "Unable to extract '%s', error when unarchiving", file.name);
 		case HNY_EXTRACTION_STATUS_ERROR_DECOMPRESSION:
 			errno = errcode;
-			err(EXIT_FAILURE, "Unable to extract %s, error when decompressing", file.name);
+			err(EXIT_FAILURE, "Unable to extract '%s', error when decompressing", file.name);
 		case HNY_EXTRACTION_STATUS_END:
 			return;
 		default: /* HNY_EXTRACTION_STATUS_OK */
@@ -74,20 +79,82 @@ hny_export(struct hny *hny, char **argpos, char **argend) {
 	}
 
 	if(readval == -1) {
-		err(EXIT_FAILURE, "Unable to read from %s", file.name);
+		err(EXIT_FAILURE, "Unable to read from '%s'", file.name);
+	}
+}
+
+static inline bool
+hny_list_is_dot_or_dot_dot(const char *name) {
+
+	return name[0] == '.' && (name[1] == '\0' || (name[1] == '.' && name[2] == '\0'));
+}
+
+static void
+hny_command_list(struct hny *hny, char **argpos, char **argend) {
+	struct {
+		unsigned packages : 1;
+		unsigned geister : 1;
+	} listing = { 0, 0 };
+	DIR *dirp;
+
+	switch(argend - argpos) {
+	case 0:
+		listing.packages = 1;
+		listing.geister = 1;
+		break;
+	case 1:
+		if(strcmp("packages", *argpos) == 0) {
+			listing.packages = 1;
+		} else if(strcmp("geister", *argpos) == 0) {
+			listing.geister = 1;
+		} else {
+			errx(EXIT_FAILURE, "list: Unexpected argument '%s'", *argpos);
+		}
+		break;
+	default:
+		errx(EXIT_FAILURE, "list: Too much arguments");
+	}
+
+	if((dirp = opendir(hny_path(hny))) != NULL) {
+		struct dirent *entry;
+
+		while((errno = 0, entry = readdir(dirp))) {
+			if(entry->d_type == DT_LNK) {
+				if(hny_type_of(entry->d_name) == HNY_TYPE_GEIST) {
+					if(listing.geister == 1) {
+						puts(entry->d_name);
+					}
+				} else {
+					warnx("list: Symbolic link entry '%s' doesn't conform to a geist name", entry->d_name);
+				}
+			} else if(entry->d_type == DT_DIR) {
+				if(!hny_list_is_dot_or_dot_dot(entry->d_name) && listing.packages == 1) {
+					if(hny_type_of(entry->d_name) == HNY_TYPE_PACKAGE) {
+						puts(entry->d_name);
+					} else {
+						warnx("list: Directory entry '%s' doesn't conform to a package name", entry->d_name);
+					}
+				}
+			} else {
+				warnx("list: Unexpected file type in prefix for '%s'", entry->d_name);
+			}
+		}
+
+		if(errno != 0) {
+			warn("list: Unable to read directory entry");
+		}
+	} else {
+		err(EXIT_FAILURE, "list: Unable to open '%s' for listing", hny_path(hny));
 	}
 }
 
 static void
-hny_remove_list(struct hny *hny, char **argpos, char **argend) {
+hny_command_remove(struct hny *hny, char **argpos, char **argend) {
 
 	if(argpos != argend) {
 		while(argpos != argend) {
-			int errcode = hny_remove(hny, *argpos);
-
-			if(errcode != 0) {
-				errno = errcode;
-				warn("Unable to remove %s", *argpos);
+			if((errno = hny_remove(hny, *argpos)) != 0) {
+				warn("Unable to remove '%s'", *argpos);
 			}
 
 			argpos++;
@@ -98,10 +165,130 @@ hny_remove_list(struct hny *hny, char **argpos, char **argend) {
 }
 
 static void
+hny_command_shift(struct hny *hny, char **argpos, char **argend) {
+
+	if(argend - argpos == 2) {
+		const char *geist = *argpos,
+			*target = argpos[1];
+
+		if(hny_type_of(geist) != HNY_TYPE_GEIST) {
+			errx(EXIT_FAILURE, "shift: '%s' is not a valid geist name", geist);
+		}
+
+		if(hny_type_of(target) == HNY_TYPE_NONE) {
+			errx(EXIT_FAILURE, "shift: '%s' is not a valid entry name", target);
+		}
+
+		if((errno = hny_shift(hny, geist, target)) != 0) {
+			errx(EXIT_FAILURE, "shift: Unable to shift '%s' to '%s'", geist, target);
+		}
+	} else {
+		errx(EXIT_FAILURE, "shift: Expected 2 arguments");
+	}
+}
+
+static int
+hny_buffer_expand(char **bufferp, size_t *sizep) {
+	char *newbuffer = realloc(*bufferp, sizeof(*newbuffer) * *sizep * 2);
+
+	if(newbuffer != NULL) {
+		*bufferp = newbuffer;
+		*sizep = *sizep * 2;
+		return 0;
+	} else {
+		return -1;
+	}
+}
+
+static int
+hny_status_resolve(int dirfd, const char *geist,
+	char **buffer1p, size_t *buffer1sizep,
+	char **buffer2p, size_t *buffer2sizep) {
+	ssize_t length;
+
+	while(stpncpy(*buffer1p, geist, *buffer1sizep) >= *buffer1p + *buffer1sizep
+		&& hny_buffer_expand(buffer1p, buffer1sizep) == 0);
+
+	if((*buffer1p)[*buffer1sizep - 1] != '\0') {
+		err(EXIT_FAILURE, "status: Unable to expand buffer");
+		return -1;
+	}
+
+	do {
+		char *swap = *buffer2p;
+		size_t swapsize = *buffer2sizep;
+
+		*buffer2p = *buffer1p;
+		*buffer2sizep = *buffer1sizep;
+		*buffer1p = swap;
+		*buffer1sizep = swapsize;
+
+		length = readlinkat(dirfd, *buffer2p, *buffer1p, *buffer1sizep);
+	} while(length != -1
+		&& (length < *buffer1sizep || hny_buffer_expand(buffer1p, buffer1sizep) == 0)
+		&& ((*buffer1p)[length] = '\0', hny_type_of(*buffer1p) == HNY_TYPE_GEIST));
+
+	if(length == -1) {
+		warn("status: Unable to readlink '%s'", *buffer2p);
+		return -1;
+	} else if(length == *buffer1sizep) {
+		err(EXIT_FAILURE, "status: Unable to expand buffer");
+		return -1;
+	} else {
+		return 0;
+	}
+}
+
+static void
+hny_command_status(struct hny *hny, char **argpos, char **argend) {
+	DIR *dirp = opendir(hny_path(hny));
+
+	if(dirp != NULL) {
+		size_t buffer1size = HNY_STATUS_BUFFER_DEFAULT_SIZE,
+			buffer2size = HNY_STATUS_BUFFER_DEFAULT_SIZE;
+		char *buffer1 = malloc(buffer1size), *buffer2 = malloc(buffer2size);
+
+		if(buffer1 == NULL || buffer2 == NULL) {
+			err(EXIT_FAILURE, "status: Unable to allocate buffer");
+		}
+
+		while(argpos != argend) {
+			if(hny_type_of(*argpos) == HNY_TYPE_GEIST) {
+				if(hny_status_resolve(dirfd(dirp), *argpos,
+					&buffer1, &buffer1size,
+					&buffer2, &buffer2size) == 0) {
+					struct stat st;
+
+					if(fstatat(dirfd(dirp), buffer1, &st, AT_SYMLINK_NOFOLLOW) == 0) {
+						if(S_ISDIR(st.st_mode)) {
+							puts(buffer1);
+						} else {
+							warnx("status: '%s' is not a directory", buffer1);
+						}
+					} else {
+						warn("status: stat '%s'", buffer1);
+					}
+				}
+			} else {
+				warnx("status: '%s' is not a valid geist name", *argpos);
+			}
+
+			argpos++;
+		}
+	} else {
+		err(EXIT_FAILURE, "status: Unable to open '%s'", hny_path(hny));
+	}
+}
+
+static void
 hny_usage(const char *hnyname, int status) {
 
-	fprintf(stderr, "usage: %s [-h] [-p <prefix>] export [filename]\n",
-		hnyname);
+	fprintf(stderr, "usage: %s [-hb] [-p <prefix>] extract [<geist>] <file>\n"
+		"       %s [-hb] [-p <prefix>] list [packages|geister]\n"
+		"       %s [-hb] [-p <prefix>] remove [<entry>...]\n"
+		"       %s [-hb] [-p <prefix>] shift <geist> <target>\n"
+		"       %s [-hb] [-p <prefix>] status [<geist>...]\n",
+		hnyname, hnyname, hnyname, hnyname, hnyname);
 	exit(status);
 }
 
@@ -136,7 +323,7 @@ hny_parse_args(int argc, char **argv) {
 	}
 
 	if((errno = hny_open(&hny, prefix, flags)) != 0) {
-		err(EXIT_FAILURE, "Unable to open honey prefix %s", prefix);
+		err(EXIT_FAILURE, "Unable to open honey prefix '%s'", prefix);
 	}
 
 	return hny;
@@ -146,8 +333,11 @@ static const struct hny_command {
 	const char *command;
 	void (*handle)(struct hny *, char **, char **);
 } hnycommands[] = {
-	{ "export", hny_export },
-	{ "remove", hny_remove_list },
+	{ "extract", hny_command_extract },
+	{ "list", hny_command_list },
+	{ "remove", hny_command_remove },
+	{ "shift", hny_command_shift },
+	{ "status", hny_command_status },
 	{ NULL, NULL },
 };
 
@@ -167,7 +357,7 @@ main(int argc, char **argv) {
 		if(current->command != NULL) {
 			current->handle(hny, argpos + 1, argend);
 		} else {
-			warnx("Invalid command %s", *argpos);
+			warnx("Invalid command '%s'", *argpos);
 			hny_usage(*argv, EXIT_FAILURE);
 		}
 	} else {
